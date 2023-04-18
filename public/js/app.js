102a7bf5b7316efb2d962a1679bd5d3fd886ec95
Manan Sharma <76532009+MananSharma7@users.noreply.github.com>
2023-04-19 02:44:02 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = "Loading...";
     messageTwo.textContent = '';
 
-    fetch("http://localhost:8080/weather?address=" + location).then((response) => {
+    fetch("https://weather-application-wheat-five.vercel.app//weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
